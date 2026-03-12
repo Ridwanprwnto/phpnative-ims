d06614a650e4d0564ed6e6daa3c11f2ae6bf1f79
Ridwanprwnto <ridwanprwnto@yahoo.com>
2026-03-12 20:23:22 +0700

    Feat: Update feature reporting BAK by Excell

--- reporting/report-history-bak.php
+++ reporting/report-history-bak.php
@@ -1,4 +1,6 @@
 <?php
+require '../vendor/autoload.php';
+
 require '../includes/config/timezone.php';
 require '../includes/function/func.php';
 require '../includes/function/tag.php';
@@ -6,6 +8,10 @@ require '../includes/config/conn.php';
 
 /*call the FPDF library*/
 require '../includes/config/mc_table.php';
+
+/*call the EXCELL library*/
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 $user = mysqli_real_escape_string($conn, $_POST["user-cetak"]);
 $office = mysqli_real_escape_string($conn, $_POST["office-cetak"]);
@@ -27,123 +33,218 @@ WHERE A.office_bkse = '$office' AND A.dept_bkse = '$dept' AND A.tgl_bkse BETWEEN
 $query_h = mysqli_query($conn, $sql);
 $header = mysqli_fetch_assoc($query_h);
 
-/*A4 width : 219mm*/
+$title = "REKAPITULASI DATA BAK PERIODE";
 
-class PDF extends PDF_MC_Table {
-    // Page header
-    function Header()
-    {
-        
-        global $header, $user, $awal, $akhir;
-        
-        /*output the result*/
-        $this->SetFont('Arial','',8);
+if (isset($_POST["printexcell"])) {
 
-        $this->Cell(28 ,5,'Kantor',0,0);
-        $this->Cell(48 ,5,': '.$header['office_bkse']." - ".$header['office_name'],0,0);
-        $this->Cell(38 ,5,'',0,0);
-        $this->Cell(28 ,5,'Print Date',0,0);
-        $this->Cell(48 ,5,': '.date("d-m-Y H:i:s"), 0, 1);
+$spreadsheet = new Spreadsheet();
+    $sheet = $spreadsheet->getActiveSheet();
 
-        $this->Cell(28 ,5,'Department',0,0);
-        $this->Cell(48 ,5,': '.$header['department_name'],0,0);
-        $this->Cell(38 ,5,'',0,0);
-        $this->Cell(28 ,5,'User Print',0,0);
-        $this->Cell(48 ,5,': '.$user, 0, 1);
+    // sheet peratama
+    $sheet->setTitle('Sheet 1');
 
-        $this->Ln(4);
+    $sheet->getStyle('A1:I1')->getFont()->setBold(true);
 
-        /*set font to arial, bold, 14pt*/
-        $this->SetFont('Arial','B',12);
+    $sheet->setCellValue('A1', 'NO');
+    $sheet->setCellValue('B1', 'DOCNO');
+    $sheet->setCellValue('C1', 'TANGGAL');
+    $sheet->setCellValue('D1', 'NAMA BARANG');
+    $sheet->setCellValue('E1', 'SERIAL NUMBER');
+    $sheet->setCellValue('F1', 'NO AKTIVA');
+    $sheet->setCellValue('G1', 'PENEMPATAN');
+    $sheet->setCellValue('H1', 'PIC');
+    $sheet->setCellValue('I1', 'KERUSAKAN');
 
-        /*Cell(width , height , text , border , end line , [align] )*/
-        $this->Cell(63 ,7,'',0,0);
-        $this->Cell(63 ,7,'REKAPITULASI DATA KERUSAKAN BARANG INVENTARIS',0,0, 'C');
-        $this->Cell(63 ,7,'',0,1);
+    $row_xcl = 2;
+    $no = 1;
 
-        $this->SetFont('Arial','',8);
+    if (isset($office) && isset($dept) && isset($awal) && isset($akhir) && isset($user) && isset($barang)) {
+    
+        $query_xcl = mysqli_query($conn, $sql);
+        if(mysqli_num_rows($query_xcl) > 0 ) {
+    
+            while($data_xcl = mysqli_fetch_assoc($query_xcl)){
 
-        $strdate_awal  =  str_replace('-"', '/', $awal);
-        $strdtstart  =  date( "d/m/Y", strtotime($strdate_awal));
-        $strdate_akhir  =  str_replace('-"', '/', $akhir);
-        $strdtstop  =  date( "d/m/Y", strtotime($strdate_akhir));
+                $slash_date  =  str_replace('-"', '/', $data_xcl['tgl_bkse']);
+                $newDate  =  date( "d/m/Y", strtotime($slash_date));
 
-        $this->Cell(190 ,5,'Periode : '.$strdtstart." - ".$strdtstop,0,1,'C');
+                $docno = $data_xcl['nomor_bkse'];
+                $desc = $data_xcl["pluid_bkse"]." - ".$data_xcl["NamaBarang"]." ".$data_xcl["NamaJenis"];
+                $sn = $data_xcl['sn_bkse'];
+                $at = $data_xcl['at_bkse'];
+                $pic = $data_xcl["user_bkse"]." - ".strtoupper($data_xcl["pic"]);
+                $lokasi = $data_xcl['penempatan_bkse'];
+                $rusak = $data_xcl['kerusakan_bkse'];
 
-        $this->Ln(4);
-
-        $this->SetFont('Arial','B',8);
-        /*Heading Of the table*/
-        $this->Cell(10 ,8,'No',1,0,'C');
-        $this->Cell(13 ,8,'Docno',1,0,'C');
-        $this->Cell(17 ,8,'Tanggal',1,0,'C');
-        $this->Cell(26 ,8,'Nama Barang',1,0,'C');
-        $this->Cell(21 ,8,'SN',1,0,'C');
-        $this->Cell(21 ,8,'AT',1,0,'C');
-        $this->Cell(21 ,8,'Penempatan',1,0,'C');
-        $this->Cell(21 ,8,'PIC',1,0,'C');
-        $this->Cell(40 ,8,'Kerusakan',1,1,'C');
-       
-    }
-
-    // Page footer
-    function Footer()
-    {
-        // Position at 1.5 cm from bottom
-        $this->SetY(-15);
-        // Arial italic 8
-        $this->SetFont('Arial','I',8);
-        // Page number
-        $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
-    }
-}
-
-$pdf = new PDF('P','mm','A4');
-$pdf->AliasNbPages();
-$pdf->AddPage();
-
-$pdf->SetTitle('Report Daftar Data Kerusakan Barang');
-
-/*Heading Of the table end*/
-$pdf->SetFont('Arial','',8);
-
-$no = 1;
-
-if (isset($office) && isset($dept) && isset($awal) && isset($akhir) && isset($user) && isset($barang)) {
-
-    $query_d = mysqli_query($conn, $sql);
-    if(mysqli_num_rows($query_d) > 0 ) {
-
-        while($data = mysqli_fetch_assoc($query_d)){
-
-            $slash_date  =  str_replace('-"', '/', $data['tgl_bkse']);
-            $newDate  =  date( "d/m/Y", strtotime($slash_date));
-
-            $docno = $data['nomor_bkse'];
-            $desc = $data["pluid_bkse"]." - ".$data["NamaBarang"]." ".$data["NamaJenis"];
-            $sn = $data['sn_bkse'];
-            $at = $data['at_bkse'];
-            $pic = $data["user_bkse"]." - ".strtoupper($data["pic"]);
-            $lokasi = $data['penempatan_bkse'];
-            $rusak = $data['kerusakan_bkse'];
-            
-            $pdf->SetWidths(array(10, 13, 17, 26, 21, 21, 21, 21, 40));
-            $pdf->Row(array($no++, $docno, $newDate, $desc, $sn, $at, $lokasi, $pic, $rusak));
-
+                $sheet->setCellValue('A'.$row_xcl, $no++);
+                $sheet->setCellValue('B'.$row_xcl, $docno);
+                $sheet->setCellValue('C'.$row_xcl, $newDate);
+                $sheet->setCellValue('D'.$row_xcl, $desc);
+                $sheet->setCellValue('E'.$row_xcl, $sn);
+                $sheet->setCellValue('F'.$row_xcl, $at);
+                $sheet->setCellValue('G'.$row_xcl, $lokasi);
+                $sheet->setCellValue('H'.$row_xcl, $pic);
+                $sheet->setCellValue('I'.$row_xcl, $rusak);
+                $row_xcl++;
+            }
+        }
+        else {
+            $msg = encrypt("datanotfound");
+            header("location: ../error.php?alert=$msg");
+            exit();
         }
     }
     else {
-        $msg = encrypt("datanotfound");
+        $msg = encrypt("print-error");
         header("location: ../error.php?alert=$msg");
         exit();
     }
+    
+    // Set active sheet index to the first sheet, so Excel opens this as the first sheet
+    $spreadsheet->setActiveSheetIndex(0);
+    
+    // Redirect output to a client’s web browser (Xlsx)
+    header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+    header('Content-Disposition: attachment; filename='.$title.' '.$awal.' '.$akhir.'.xlsx');
+    header('Cache-Control: max-age=0');
+    // If you're serving to IE 9, then the following may be needed
+    header('Cache-Control: max-age=1');
+    
+    // If you're serving to IE over SSL, then the following may be needed
+    header('Expires: Mon, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+    header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT'); // always modified
+    header('Cache-Control: cache, must-revalidate'); // HTTP/1.1
+    header('Pragma: public'); // HTTP/1.0
+
+    $writer = new Xlsx($spreadsheet);
+    $writer->save('php://output');
+
+}
+elseif (isset($_POST["printpdf"])) {
+
+    /*A4 width : 219mm*/
+    class PDF extends PDF_MC_Table {
+        // Page header
+        function Header()
+        {
+            
+            global $header, $user, $awal, $akhir;
+            
+            /*output the result*/
+            $this->SetFont('Arial','',8);
+
+            $this->Cell(28 ,5,'Kantor',0,0);
+            $this->Cell(48 ,5,': '.$header['office_bkse']." - ".$header['office_name'],0,0);
+            $this->Cell(38 ,5,'',0,0);
+            $this->Cell(28 ,5,'Print Date',0,0);
+            $this->Cell(48 ,5,': '.date("d-m-Y H:i:s"), 0, 1);
+
+            $this->Cell(28 ,5,'Department',0,0);
+            $this->Cell(48 ,5,': '.$header['department_name'],0,0);
+            $this->Cell(38 ,5,'',0,0);
+            $this->Cell(28 ,5,'User Print',0,0);
+            $this->Cell(48 ,5,': '.$user, 0, 1);
+
+            $this->Ln(4);
+
+            /*set font to arial, bold, 14pt*/
+            $this->SetFont('Arial','B',12);
+
+            /*Cell(width , height , text , border , end line , [align] )*/
+            $this->Cell(63 ,7,'',0,0);
+            $this->Cell(63 ,7,'REKAPITULASI DATA KERUSAKAN BARANG INVENTARIS',0,0, 'C');
+            $this->Cell(63 ,7,'',0,1);
+
+            $this->SetFont('Arial','',8);
+
+            $strdate_awal  =  str_replace('-"', '/', $awal);
+            $strdtstart  =  date( "d/m/Y", strtotime($strdate_awal));
+            $strdate_akhir  =  str_replace('-"', '/', $akhir);
+            $strdtstop  =  date( "d/m/Y", strtotime($strdate_akhir));
+
+            $this->Cell(190 ,5,'Periode : '.$strdtstart." - ".$strdtstop,0,1,'C');
+
+            $this->Ln(4);
+
+            $this->SetFont('Arial','B',8);
+            /*Heading Of the table*/
+            $this->Cell(10 ,8,'No',1,0,'C');
+            $this->Cell(13 ,8,'Docno',1,0,'C');
+            $this->Cell(17 ,8,'Tanggal',1,0,'C');
+            $this->Cell(26 ,8,'Nama Barang',1,0,'C');
+            $this->Cell(21 ,8,'SN',1,0,'C');
+            $this->Cell(21 ,8,'AT',1,0,'C');
+            $this->Cell(21 ,8,'Penempatan',1,0,'C');
+            $this->Cell(21 ,8,'PIC',1,0,'C');
+            $this->Cell(40 ,8,'Kerusakan',1,1,'C');
+        
+        }
+
+        // Page footer
+        function Footer()
+        {
+            // Position at 1.5 cm from bottom
+            $this->SetY(-15);
+            // Arial italic 8
+            $this->SetFont('Arial','I',8);
+            // Page number
+            $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+        }
+    }
+
+    $pdf = new PDF('P','mm','A4');
+    $pdf->AliasNbPages();
+    $pdf->AddPage();
+
+    $pdf->SetTitle('Report Daftar Data Kerusakan Barang');
+
+    /*Heading Of the table end*/
+    $pdf->SetFont('Arial','',8);
+
+    $no = 1;
+
+    if (isset($office) && isset($dept) && isset($awal) && isset($akhir) && isset($user) && isset($barang)) {
+
+        $query_d = mysqli_query($conn, $sql);
+        if(mysqli_num_rows($query_d) > 0 ) {
+
+            while($data = mysqli_fetch_assoc($query_d)){
+
+                $slash_date  =  str_replace('-"', '/', $data['tgl_bkse']);
+                $newDate  =  date( "d/m/Y", strtotime($slash_date));
+
+                $docno = $data['nomor_bkse'];
+                $desc = $data["pluid_bkse"]." - ".$data["NamaBarang"]." ".$data["NamaJenis"];
+                $sn = $data['sn_bkse'];
+                $at = $data['at_bkse'];
+                $pic = $data["user_bkse"]." - ".strtoupper($data["pic"]);
+                $lokasi = $data['penempatan_bkse'];
+                $rusak = $data['kerusakan_bkse'];
+                
+                $pdf->SetWidths(array(10, 13, 17, 26, 21, 21, 21, 21, 40));
+                $pdf->Row(array($no++, $docno, $newDate, $desc, $sn, $at, $lokasi, $pic, $rusak));
+
+            }
+        }
+        else {
+            $msg = encrypt("datanotfound");
+            header("location: ../error.php?alert=$msg");
+            exit();
+        }
+    }
+    else {
+        $msg = encrypt("print-error");
+        header("location: ../error.php?alert=$msg");
+        exit();
+    }
+
+    $pdf->Output($title." ".$awal." ".$akhir.".pdf","I");
+
 }
 else {
     $msg = encrypt("print-error");
     header("location: ../error.php?alert=$msg");
     exit();
 }
-
-$pdf->Output("REKAPITULASI DATA BAK PERIODE ".$awal." ".$akhir.".pdf","I");
 
 ?>
